9ca6c40bd452224ca6163ee081c4bf6d0fad71db
Vishal-Shandilya17334 <visushandilya@gmail.com>
2024-05-10 01:44:29 +0530

    selectionSort

--- ShortingAlog/SelectionSort.c
+++ ShortingAlog/SelectionSort.c
@@ -1,44 +1,55 @@
+/* Selection sort algorithm 
+   Complexcity of algoritm is = O(n2)
+   n is the number of elements in the array being sorted.
+
+The outer loop runs 
+n−1 times, iterating through each element of the array except for the last one.
+The inner loop runs 
+n−i−1 times in each iteration of the outer loop, where 
+
+*/ 
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
-    if (n <= 0) {
+void selectionSort(int arr[],int N)
+       {
+            if (N <= 0) {
         printf("Array is empty, no sorting needed.\n");
         return;
     }
-
-    int i, j, minIndex, temp;
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+            int i,j,MinIndex,temp;   // useful variable to use for storing value
+            for(i=0;i<N-1;i++)   // full array iterations n-1
+            {
+                MinIndex = i;       // store first value of array 
+                for(j=i+1;j<N;j++)
+                {
+                    if(arr[MinIndex]>arr[j])  // check first one is large from seconde one if than store 2nd index
+                    {
+                        MinIndex = j;
+                    }
+                   
+                }
+                if(MinIndex!=i)   // if MinIndex and i both are zero means no array if not than swap
+                                  //  one by one
+                {
+                    temp = arr[i];
+                    arr[i] = arr[MinIndex];
+                    arr[MinIndex] = temp;
+                }
             }
-        }
-        if (minIndex != i) {
-            temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
-}
-
-int main() {
-    int arr[5] = {}; // This array is currently empty
-    int n = sizeof(arr) / sizeof(arr[0]);
-     if (n == 0) {
-        printf("Array is empty, no sorting needed.\n");
-    } else {
-        selectionSort(arr, n);
-        printf("Sorted array: \n");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-    //selectionSort(arr, n);
-    }
-    printf("Sorted array: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 0;
+       }
+int main()
+{
+    int list[5];
+    int N;
+    printf("Befor shorting\n");
+    for(int i=0; i<5; i++)
+     printf("%d\t",list[i]);
+     printf("\n");
+     N = sizeof(list)/sizeof(list[0]);
+     selectionSort(list,N);
+     printf("After sorting --->>>\n");
+     for(int i=0; i<5; i++)
+     printf("%d\t",list[i]);
+     printf("\n");
+   
 }
